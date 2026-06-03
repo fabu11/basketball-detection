cca7ba626493ed5881becffe5d41f4334e28bc7e
Akshat Khandelwal <akshatk1001@gmail.com>
2026-06-03 09:45:29 -0700

    use makemiss2 to determine shot made in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from human_position import pixel_to_court_coords, compute_homography, overlay_court_coords_on_frame, smallvids_frame_points, court_rw_points, longvids_frame_points
 from ball_detector import draw_ball_on_frame
 from utils import load_video, display_frames_video, apply_color_filter
-from makemiss import makemiss
+from makemiss import makemiss, makemiss2
 
 frames = load_video("shots/longvids/1.mov", display=False)
 h = compute_homography(longvids_frame_points, court_rw_points)
@@ -21,7 +21,7 @@ for i, f in enumerate(frames):
 # get the frame indices where the ball went through the hoop
 smallvids_hoop_coords = (971, 377)
 longvids_hoop_coords = (647, 245)
-make_frames, miss_frames = makemiss(longvids_hoop_coords, ballpos_list)
+make_frames, miss_frames = makemiss2(longvids_hoop_coords, ballpos_list)
 apply_color_filter(annotated, make_frames, (0, 255, 0))
 apply_color_filter(annotated, miss_frames, (0, 0, 255))
 
